a150ddd0fe34750235012f6229bb4abaa8ca61d0
obie <oscar@agiledigital.com.ph>
2026-05-27 01:27:20 +0800

    Initial Commit

---
+++ CommuteBeh/CommuteBehApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommuteBehApp.swift
+//  CommuteBeh
+//
+//  Created by Oscar Allen Brioso on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CommuteBehApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
